aa1b91fb85e0b8e4336dda590cb1700376f4aaec
holako25 <holakoledar@email.com>
2026-04-19 23:56:54 +0300

    update invoice ai

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "sb_publishable_T4ZnLiPeIQ6-HylURhD5eQ_bjFY7LbT";
-const supabaseKey = "sb_secret_KeOec8IfoIRIxIEigtnt6A_SrTBsU16";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
